d49553a877008c3f840265778c9411189f2a054b
Rubick1337 <poss1337@gmail.com>
2025-08-18 00:46:24 +0300

    feat: base index.js server for work

--- tg-web-app-bot-server/index.js
+++ tg-web-app-bot-server/index.js
@@ -3,15 +3,25 @@ require("dotenv").config()
 const express = require('express');
 const app = express()
 const cors = require('cors');
-
+const webAppUrl = 'https://12fb37e03f41.ngrok-free.app'
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
 
 app.use(express.json());
 app.use(cors());
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
-    bot.sendMessage(chatId, text);
-})
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, войдите в систему', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: "Войти в систему" ,web_app: {url: webAppUrl}}]
+                ],
+                resize_keyboard: true
+            }
+        });
+    }
+});
